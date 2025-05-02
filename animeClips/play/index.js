9b9f5133ee09b95791fd4388f7e9e92a0ec7f3f4
syhy <3519450570@qq.com>
2025-05-02 21:03:52 +0800

    Site updated: 2025-05-02 21:03:46

--- animeClips/play/index.js
+++ animeClips/play/index.js
@@ -1,5 +1,20 @@
-const urlParams = new URLSearchParams(location.search)
-document.addEventListener('DOMContentLoaded',()=>{
- const video = document.querySelector('#article-container video')
- video.src = `/animeClips/video/${urlParams.get('video')}.mp4`
-})
+if (/\/animeClips\/play/.test(location.pathname)) {
+  //使用URLSearchParams,能很快的从查询参数中取值，前提是你知道有哪些参数
+  const urlParams = new URLSearchParams(location.search)
+  const title = urlParams.get('title')
+  const video = urlParams.get('video')
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const v = document.querySelector('#article-container video')
+    v.src = `/animeClips/video/${video}.mp4`
+    //支持自定义标题
+    document.querySelector('.page-title').innerHTML = `<i class="fa-solid fa-arrow-left" onclick="history.go(-1)"></i>${title ? title : video}`
+  })
+}else{
+  //给标题部分添加返回按钮，使用于所有不使用top_img的页面
+  const title =  document.querySelector('.page-title')
+  if(title){
+    let a = title.innerHTML
+    title.innerHTML = `<i class="fa-solid fa-arrow-left" onclick="history.go(-1)"></i>`+a
+  }
+}
